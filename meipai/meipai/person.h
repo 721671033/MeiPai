c6a3b9719e29ad8a9a80bf5fea4e7259c060c5c4
LYN <721671033@qq.com>
2017-03-10 10:42:01 +0800

    person

---
+++ meipai/meipai/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  meipai
+//
+//  Created by lyn on 17/3/10.
+//  Copyright © 2017年 lyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ meipai/meipai/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  meipai
+//
+//  Created by lyn on 17/3/10.
+//  Copyright © 2017年 lyn. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
