251044de3928321deb19fdb7f5f52178881a0938
LYN <721671033@qq.com>
2017-03-10 10:40:24 +0800

    初始化项目

---
+++ meipai/meipai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meipai
+//
+//  Created by lyn on 17/3/10.
+//  Copyright © 2017年 lyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
